c95d507d7bf6ac31c949570d7f692e67f993558d
Kévin Roussel <roussel.kevin@outlook.com>
2019-04-26 21:45:04 +0200

    Start Project Structure

---
+++ Assets/Master.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    Coroutine _game;
+
+    void Awake()
+    {
+        StartCoroutine(Game());
+    }
+
+    IEnumerator Game()
+    {
+
+        yield break;
+    }
+
+
+}
