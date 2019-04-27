cbdf4b599b03006982a3da33bfca4f17df722203
Ben <benjaminr@laposte.net>
2019-04-27 13:03:07 +0200

    Very beginning of AI/Navigation

---
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseEnemy : MonoBehaviour {
+
+    NavMeshAgent _navMeshAgent;
+
+    void Start () {
+        
+        if(!GetComponentInChildren<NavMeshAgent>())
+           gameObject.AddComponent<NavMeshAgent>();
+
+        _navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+
+        _navMeshAgent.SetDestination(new Vector3(0, .5f, 0));
+
+    }
+
+}
