812855d6c795640a56d4dfac51a1567170577cda
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-04-27 20:59:12 +0200

    Basic ending decision

---
+++ Assets/Scripts/EndingDecision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingDecision : MonoBehaviour
+{
+    [SerializeField] int evilPoints;
+
+    [SerializeField] int badEndingThreshold;
+
+    // TODO : Delete Start function and integrate ending decision in the coroutine process
+    void Start()
+    {
+        if (evilPoints > badEndingThreshold) {
+            CallBadEnding();
+        } else {
+            CallGoodEnding();
+        }
+    }
+
+    void CallBadEnding() {
+        Debug.Log("Bad Ending");
+    }
+
+    void CallGoodEnding() {
+        Debug.Log("Good Ending");
+    }
+}
