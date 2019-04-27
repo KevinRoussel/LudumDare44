d4abe37e109b37ad23fd225cabb130f5f4d01ebe
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-04-27 15:03:22 +0200

    Usable dialog UI prototype

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    [SerializeField] Text name;
+    [SerializeField] Text dialogText;
+
+    public void ChangeDialog(string newName, string newDialogText) {
+        this.gameObject.SetActive(true);
+        this.ChangeName(newName);
+        this.ChangeDialogText(newDialogText);
+    }
+
+    public void ChangeName(string name) {
+        this.name.text = name;
+    }
+
+    public void ChangeDialogText(string dialogText) {
+        this.dialogText.text = dialogText;
+    }
+}
