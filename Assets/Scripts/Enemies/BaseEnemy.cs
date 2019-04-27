23954f76e469abce743551b804414f3ea38fc514
Ben <benjaminr@laposte.net>
2019-04-27 14:07:00 +0200

    Base AI movement

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -3,16 +3,46 @@ using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour {
 
+    EnemiesManager _enemiesManager;
+
+    [Header("Navigation variables")]
+    [Tooltip("Maximum distance for the enemy to chase the player")]
+    [SerializeField] float _playerDetectionDistance;
+
+    [Tooltip("Range for the distance of the next wandering position from current position")]
+    [SerializeField] Vector2 _nextWanderingPosRange;
+
     NavMeshAgent _navMeshAgent;
 
+    GameObject _player;
+
     void Start () {
-        
-        if(!GetComponentInChildren<NavMeshAgent>())
-           gameObject.AddComponent<NavMeshAgent>();
 
-        _navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+        _enemiesManager = FindObjectOfType<EnemiesManager>();
 
-        _navMeshAgent.SetDestination(new Vector3(0, .5f, 0));
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        _enemiesManager.Enemies.Add(this);
+
+    }
+
+    public void Movement () {
+
+        if (Vector3.Distance(_player.transform.position, transform.position) <= _playerDetectionDistance)
+            _navMeshAgent.SetDestination(_player.transform.position);
+        else if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance) {
+
+            Vector2 randomPosOnCircle = Random.insideUnitCircle;
+
+            Vector3 randomPointInRange = new Vector3(randomPosOnCircle.x, 0, randomPosOnCircle.y) * Random.Range(_nextWanderingPosRange.x, _nextWanderingPosRange.y);
+
+            NavMesh.SamplePosition(transform.position + randomPointInRange, out NavMeshHit hit, _nextWanderingPosRange.y, NavMesh.AllAreas);
+
+            _navMeshAgent.SetDestination(hit.position);
+
+        }
 
     }
 
