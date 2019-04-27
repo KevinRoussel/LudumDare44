15d639db038d10b428ac8d05e462ed7f4179e88c
Kévin Roussel <roussel.kevin@outlook.com>
2019-04-27 21:35:05 +0200

    New Camera component

---
+++ Assets/PlayerTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    [SerializeField] Vector3 _offset;
+
+    Character _player;
+
+    private void Update()
+    {
+        if(_player == null)
+        {
+            _player = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Character>();
+        }
+         
+        if(_player!=null)
+        {
+            transform.position = _player.transform.position + _offset;
+            transform.LookAt(_player.transform);
+        }
+
+    }
+
+
+}
