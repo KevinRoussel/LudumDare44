c1232da3431177e59f2a300f44a71d5e0dfe47c0
Ben <benjaminr@laposte.net>
2019-04-27 16:36:34 +0200

    Shooting, Work on Base Enemy

--- Assets/Scripts/Enemies/EnemiesManager.cs
+++ Assets/Scripts/Enemies/EnemiesManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EnemiesManager : MonoBehaviour {
 
     private readonly List<BaseEnemy> _enemies = new List<BaseEnemy>();
-
     public List<BaseEnemy> Enemies => _enemies;
 
     // Will eventually be deleted and changed into a Coroutine called by Master
