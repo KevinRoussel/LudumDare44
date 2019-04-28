71baf42f774bc2ba8469b1c63414ed592c30d6b6
Kévin Roussel <roussel.kevin@outlook.com>
2019-04-29 00:38:56 +0200

    FXOnEvent

---
+++ Assets/FXOnEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXOnEvent : MonoBehaviour
+{
+    [Header("Master")]
+    [SerializeField] Character _target;
+
+    [Header("SFX")]
+    ParticleSystem _rageActivated;
+
+    private void Start()
+    {
+        _target.OnRageReady += () => _rageActivated.Play();
+
+
+
+    }
+
+}
