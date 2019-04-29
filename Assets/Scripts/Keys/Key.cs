332a5fb37e68df0cd97c10035ac9719af48c8b61
Kévin Roussel <roussel.kevin@outlook.com>
2019-04-29 10:41:20 +0200

    Key Effect Collecting

--- Assets/Scripts/Keys/Key.cs
+++ Assets/Scripts/Keys/Key.cs
@@ -1,17 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Key : MonoBehaviour {
 
+    [SerializeField] MeshRenderer _renderer;
+    [SerializeField] ParticleSystem _collectingKeyEffect;
+    [SerializeField] Light _collectingLight;
+
+    Coroutine collecting;
     void OnTriggerEnter (Collider other) {
         
         if(other.CompareTag("Player")) {
 
             FindObjectOfType<KeysManager>().KeyCollected();
-
-            Destroy(gameObject);
-
+            if(collecting==null) collecting = StartCoroutine(Collecting());
         }
 
+    }
+
+    IEnumerator Collecting()
+    {
+        _renderer.enabled = false;
+        _collectingKeyEffect.Play();
+
+        _collectingLight.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        _collectingLight.enabled = false;
+
+        yield return new WaitForSeconds(1f);
+
+
+        Destroy(gameObject);
+        yield break;
     }
 
 }
