0c951f241b7f7942a3f5602885dbf2d5782faf32
Kévin Roussel <roussel.kevin@outlook.com>
2019-05-03 18:26:51 +0200

    Implement Pause Button

---
+++ Assets/KeyTriggerInsteadButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyTriggerInsteadButton : MonoBehaviour
+{
+    // Hard dirty key updating i know #ShameOnMe
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.P)) GetComponent<Button>().onClick.Invoke();
+    }
+}
